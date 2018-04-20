fde49b3d265116aaea9709552172ef0261676088
Flavio Milani <milanidesign7@gmail.com>
2018-04-20 16:02:29 -0300

    ajustando server js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
-module.exports = {
-	port: process.env.PORT
-}
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
